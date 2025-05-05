5942e81d0d37ae15068cd7051f50a8ba56066b88
KarthikeyanQA-K <karthikjoseph4041@gmail.com>
2025-05-05 15:58:52 +0530

    Update LoginTest.java

--- Samplee/src/test/java/LoginTest.java
+++ Samplee/src/test/java/LoginTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
         
 
         // Initialize WebDriver
