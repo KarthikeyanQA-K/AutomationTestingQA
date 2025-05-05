0b5126827633d46ad8109d6da5ce6b485e366efc
KarthikeyanQA-K <karthikjoseph4041@gmail.com>
2025-05-05 16:16:21 +0530

    int

---
+++ Samplee/src/test/java/Test/loginTest.java
@@ -0,0 +1,24 @@
+package Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class loginTest {
+	@Test
+	 public void login() {
+	        
+
+	        // Initialize WebDriver
+	        WebDriver driver = new ChromeDriver();
+
+	       
+	            // Open the login page
+	            driver.get("https://www.google.com");
+
+	            // Maximize the browser window
+	            driver.manage().window().maximize();
+System.out.println("suuesss");
+	    }
+	}
+
